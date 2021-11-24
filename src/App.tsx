528a9e92fa21eec542f65e9cbc44c6d427622cbb
belanglos <benjamin@longstream.se>
2021-11-24 11:33:51 +0000

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+// import OrganisationSearch from './OrganisationSearch';
+import PersonList from './components/PersonList';
+
+function App() {
+	return <PersonList />;
+}
+
+export default App;
