0573368ef670ea36a37ddc07916ce3ca60a484a2
belanglos <benjamin@longstream.se>
2021-11-26 11:05:22 +0000

    Started implementing convertPerson.

---
+++ __tests__/converter/Converter.spec.ts
@@ -0,0 +1,140 @@
+import Person from '../../src/control/Person';
+import convertPerson from '../../src/converter/Converter';
+import { DataGroup } from '../../src/converter/CoraData';
+
+describe('The functions in converter can be used to convert DataGroups to TS-objects', () => {
+	describe('convertPerson', () => {
+		it('returns an empty Person if all fields were empty', () => {
+			const person: Person = convertPerson(emptyPersonDataGroup);
+
+			expect(person.authorisedName.familyName).toStrictEqual('');
+			expect(person.authorisedName.givenName).toStrictEqual('');
+			expect(person.id).toStrictEqual('');
+		});
+
+		it('Sets the ID', () => {
+			const person: Person = convertPerson(personDataGroupWithId);
+
+			expect(person.authorisedName.familyName).toStrictEqual('');
+			expect(person.authorisedName.givenName).toStrictEqual('');
+			expect(person.id).toStrictEqual('someId');
+		});
+
+		it('Sets empty name if DataGroup for name does not exist', () => {
+			const person: Person = convertPerson(personDataGroupWithOnlyId);
+
+			expect(person.authorisedName.familyName).toStrictEqual('');
+			expect(person.authorisedName.givenName).toStrictEqual('');
+			expect(person.id).toStrictEqual('someId');
+		});
+
+		it('Sets the authorisedName', () => {
+			const person: Person = convertPerson(
+				personDataGroupWithIdAndAuthorisedName
+			);
+
+			expect(person.authorisedName.familyName).toStrictEqual('SomeFamilyName');
+			expect(person.authorisedName.givenName).toStrictEqual('SomeGivenName');
+			expect(person.id).toStrictEqual('someId');
+		});
+	});
+});
+
+const emptyPersonDataGroup: DataGroup = {
+	name: 'person',
+	children: [
+		{
+			name: 'recordInfo',
+			children: [
+				{
+					name: 'id',
+					value: '',
+				},
+			],
+		},
+		{
+			children: [
+				{
+					name: 'familyName',
+					value: '',
+				},
+				{
+					name: 'givenName',
+					value: '',
+				},
+			],
+			name: 'authorisedName',
+		},
+	],
+};
+
+const personDataGroupWithId: DataGroup = {
+	name: 'person',
+	children: [
+		{
+			name: 'recordInfo',
+			children: [
+				{
+					name: 'id',
+					value: 'someId',
+				},
+			],
+		},
+		{
+			children: [
+				{
+					name: 'familyName',
+					value: '',
+				},
+				{
+					name: 'givenName',
+					value: '',
+				},
+			],
+			name: 'authorisedName',
+		},
+	],
+};
+
+const personDataGroupWithIdAndAuthorisedName: DataGroup = {
+	name: 'person',
+	children: [
+		{
+			name: 'recordInfo',
+			children: [
+				{
+					name: 'id',
+					value: 'someId',
+				},
+			],
+		},
+		{
+			children: [
+				{
+					name: 'familyName',
+					value: 'SomeFamilyName',
+				},
+				{
+					name: 'givenName',
+					value: 'SomeGivenName',
+				},
+			],
+			name: 'authorisedName',
+		},
+	],
+};
+
+const personDataGroupWithOnlyId: DataGroup = {
+	name: 'person',
+	children: [
+		{
+			name: 'recordInfo',
+			children: [
+				{
+					name: 'id',
+					value: 'someId',
+				},
+			],
+		},
+	],
+};
--- src/converter/Converter.ts
+++ src/converter/Converter.ts
@@ -1,14 +1,60 @@
+import Name from '../control/Name';
 import Person from '../control/Person';
-import { DataGroup } from './CoraData';
+import { DataAtomic, DataGroup } from './CoraData';
+import { getFirstChildWithNameInData } from './CoraDataUtils';
 
 export function convertPerson(dataGroup: DataGroup): Person {
+	const id: string = extractIdFromDataGroup(dataGroup);
+
+	const authorisedName: Name =
+		extractAuthorisedNameFromPersonDataGroup(dataGroup);
+
 	return {
-		authorisedName: {
-			familyName: 'bar',
-			givenName: 'foo',
-		},
-		id: '2',
+		authorisedName,
+		id,
 	};
+}
+
+function extractIdFromDataGroup(dataGroup: DataGroup): string {
+	const recordInfo: DataGroup = <DataGroup>(
+		getFirstChildWithNameInData(dataGroup, 'recordInfo')
+	);
+
+	const idAtomic: DataAtomic = <DataAtomic>(
+		getFirstChildWithNameInData(recordInfo, 'id')
+	);
+
+	return idAtomic.value;
+}
+
+function extractAuthorisedNameFromPersonDataGroup(
+	personDataGroup: DataGroup
+): Name {
+	const nameToReturn: Name = {
+		familyName: '',
+		givenName: '',
+	};
+
+	try {
+		const authorisedName: DataGroup = <DataGroup>(
+			getFirstChildWithNameInData(personDataGroup, 'authorisedName')
+		);
+
+		const familyName: DataAtomic = <DataAtomic>(
+			getFirstChildWithNameInData(authorisedName, 'familyName')
+		);
+
+		const givenName: DataAtomic = <DataAtomic>(
+			getFirstChildWithNameInData(authorisedName, 'givenName')
+		);
+
+		nameToReturn.familyName = familyName.value;
+		nameToReturn.givenName = givenName.value;
+	} catch (error) {
+		//TODO: decide what to do here...
+	}
+
+	return nameToReturn;
 }
 
 export default convertPerson;
