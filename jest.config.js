9ebfb4af7d9e9f71f92052fc0deab221c469ca71
belanglos <benjamin@longstream.se>
2021-12-02 09:12:52 +0000

    Add better support for coverage.

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['./node_modules/'],
-  setupFilesAfterEnv: [
-	"<rootDir>/jest.setup.js"
-  ]
+	preset: 'ts-jest',
+	testEnvironment: 'jsdom',
+	testPathIgnorePatterns: ['./node_modules/'],
+	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+	collectCoverage: true,
+	collectCoverageFrom: ['./src/**'],
+	coverageThreshold: {
+		global: {
+			statements: 100,
+			branches: 100,
+			functions: 100,
+			lines: 100,
+		},
+	},
 };
