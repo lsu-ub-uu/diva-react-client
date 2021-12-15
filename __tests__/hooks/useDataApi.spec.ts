ba53b6924107b401c8ece27e7c1e376151dc5ed9
belanglos <benjamin@longstream.se>
2021-12-15 11:05:32 +0000

    Tested hook in place.

--- __tests__/hooks/useDataApi.spec.ts
+++ __tests__/hooks/useDataApi.spec.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import axios from 'axios';
-import { renderHook } from '@testing-library/react-hooks/dom';
+import { renderHook, act } from '@testing-library/react-hooks/dom';
 import useDataApi from '../../src/hooks/useDataApi';
 
 jest.mock('axios');
@@ -8,7 +8,7 @@ jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 beforeAll(() => {
-	mockedAxios.get.mockResolvedValue({ data: 'someDefaultData' });
+	mockedAxios.get.mockResolvedValue({ someField: 'someDefaultData' });
 });
 
 describe('The useDataApi hook', () => {
@@ -16,79 +16,215 @@ describe('The useDataApi hook', () => {
 		renderHook(() => useDataApi('', ''));
 	});
 
-	it('should return data, isLoading, isError as well as setUrl', async () => {
+	it('should return data, isLoading, errors as well as setUrl', async () => {
 		const { result } = renderHook(() =>
-			useDataApi('', { data: 'someInitialData' })
+			useDataApi('', { someField: 'someInitialData' })
 		);
 
-		expect(result.current[0].data).toStrictEqual({ data: 'someInitialData' });
+		expect(result.current[0].data).toStrictEqual({
+			someField: 'someInitialData',
+		});
 		expect(result.current[0].isLoading).toBeTruthy();
-		expect(result.current[0].isError).toBeFalsy();
+		expect(result.current[0].error).toBeNull();
 		expect(result.current[1]).toBeDefined();
 	});
 
 	it('should not call axios if initialUrl is empty', async () => {
 		renderHook(() => useDataApi('', ''));
 
-		expect(mockedAxios).toHaveBeenCalledTimes(0);
+		expect(mockedAxios.get).toHaveBeenCalledTimes(0);
 	});
 
-	it('should call axios if initialUrl is provided', async () => {
-		renderHook(() => useDataApi('someUrl', ''));
+	describe('if initialURL is provided', () => {
+		it('should call axios with that URL', async () => {
+			const { result, waitFor } = renderHook(() => useDataApi('someUrl', ''));
 
-		expect(mockedAxios).toHaveBeenCalledTimes(1);
-		expect(mockedAxios).toHaveBeenCalledWith('someUrl');
+			expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+			expect(mockedAxios.get).toHaveBeenCalledWith('someUrl');
+
+			await waitFor(() => {
+				expect(result.current[0].isLoading).toBeFalsy();
+			});
+		});
+
+		it('should change loading state if axios call is successful', async () => {
+			mockedAxios.get.mockImplementationOnce(() =>
+				Promise.resolve({
+					status: 200,
+					data: 'data',
+				})
+			);
+			const { result, waitFor } = renderHook(() => useDataApi('someUrl', ''));
+
+			await waitFor(() => {
+				expect(result.current[0].isLoading).toBeFalsy();
+			});
+		});
+
+		it('should return data from axios if call is successful', async () => {
+			mockedAxios.get.mockResolvedValueOnce({
+				data: { someField: 'someData' },
+			});
+			const { result, waitFor } = renderHook(() =>
+				useDataApi('someUrl', { someField: 'initialData' })
+			);
+
+			await waitFor(() => {
+				expect(result.current[0].data).toStrictEqual({ someField: 'someData' });
+			});
+		});
+
+		it('should return data from axios if call is successful 2', async () => {
+			mockedAxios.get.mockResolvedValueOnce({
+				data: { someField: 'someOtherData' },
+			});
+			const { result, waitFor } = renderHook(() =>
+				useDataApi('someUrl', { someField: 'initialData' })
+			);
+
+			await waitFor(() => {
+				expect(result.current[0].data).toStrictEqual({
+					someField: 'someOtherData',
+				});
+			});
+		});
 	});
 
-	it('should change loading state if axios call is successful', async () => {
-		// mockedAxios.get.mockResolvedValueOnce({ data: 'data' });
-		mockedAxios.get.mockImplementationOnce(() =>
-			Promise.resolve({
-				status: 200,
-				data: 'data',
-			})
-		);
+	describe('should handle errors', () => {
+		it('should change loading state, set error and not change data if axios call is unsuccessful', async () => {
+			mockedAxios.get.mockRejectedValueOnce({
+				response: 'someResponse',
+			});
+			const { result, waitFor } = renderHook(() => useDataApi('someUrl', ''));
+
+			await waitFor(() => {
+				expect(result.current[0].isLoading).toBeFalsy();
+				expect(result.current[0].error).not.toBeNull();
+				expect(result.current[0].data).toStrictEqual('');
+			});
+		});
+
+		it('should populate error message if axios returns response', async () => {
+			mockedAxios.get.mockRejectedValueOnce({
+				response: {
+					data: 'Some error message',
+					status: 404,
+					headers: {},
+				},
+			});
+			const { result, waitFor } = renderHook(() => useDataApi('someUrl', ''));
+
+			await waitFor(() => {
+				expect(result.current[0].error).not.toBeNull();
+				expect(result.current[0].error?.message).toStrictEqual(
+					'Some error message'
+				);
+			});
+		});
+
+		it('should populate error message if axios returns response 2', async () => {
+			mockedAxios.get.mockRejectedValueOnce({
+				response: {
+					data: 'Some other error message',
+					status: 404,
+					headers: {},
+				},
+			});
+			const { result, waitFor } = renderHook(() => useDataApi('someUrl', ''));
+
+			await waitFor(() => {
+				expect(result.current[0].error).not.toBeNull();
+				expect(result.current[0].error?.message).toStrictEqual(
+					'Some other error message'
+				);
+			});
+		});
+
+		it('should populate error message if axios returns XMLHttpRequest', async () => {
+			mockedAxios.get.mockRejectedValueOnce({
+				request: 'Some XMLHttpRequest',
+			});
+			const { result, waitFor } = renderHook(() => useDataApi('someUrl', ''));
+
+			await waitFor(() => {
+				expect(result.current[0].error).not.toBeNull();
+				expect(result.current[0].error?.message).toStrictEqual(
+					'The request was made but no response was received.'
+				);
+			});
+		});
+
+		it('should populate error message if axios returns general error', async () => {
+			mockedAxios.get.mockRejectedValueOnce({
+				message: 'Some general error message',
+			});
+			const { result, waitFor } = renderHook(() => useDataApi('someUrl', ''));
+
+			await waitFor(() => {
+				expect(result.current[0].error).not.toBeNull();
+				expect(result.current[0].error?.message).toStrictEqual(
+					'Some general error message'
+				);
+			});
+		});
+	});
+
+	it('should call axios again if called with different URL', async () => {
 		const { result, waitFor } = renderHook(() => useDataApi('someUrl', ''));
 
-		expect(mockedAxios).toHaveBeenCalledTimes(1);
-		expect(mockedAxios).toHaveBeenCalledWith('someUrl');
-		await waitFor(
-			() => {
-				expect(result.current[0].isLoading).toBeFalsy();
-			},
-			{ interval: 500 }
-		);
-	});
-
-	it('should return data from axios if call is successful', async () => {
-		mockedAxios.get.mockResolvedValueOnce({ data: 'someData' });
-		const { result, waitFor } = renderHook(() =>
-			useDataApi('someUrl', { data: 'initialData' })
-		);
-
-		const [{ data, isLoading }] = result.current;
+		expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+		expect(mockedAxios.get).toHaveBeenCalledWith('someUrl');
 
 		await waitFor(() => {
-			expect(isLoading).toBeFalsy();
-			expect(data).toStrictEqual({ data: 'someData' });
-			console.log(data);
+			expect(result.current[0].isLoading).toBeFalsy();
+		});
+
+		const setUrl = result.current[1];
+
+		act(() => {
+			setUrl('someOtherUrl');
+		});
+
+		expect(result.current[0].isLoading).toBeTruthy();
+
+		expect(mockedAxios.get).toHaveBeenCalledTimes(2);
+		expect(mockedAxios.get).toHaveBeenCalledWith('someOtherUrl');
+
+		await waitFor(() => {
+			expect(result.current[0].isLoading).toBeFalsy();
 		});
 	});
 
-	it('should return data from axios if call is successful 2', async () => {
-		mockedAxios.get.mockResolvedValueOnce({ data: 'someOtherData' });
-		const { result, waitFor } = renderHook(() =>
-			useDataApi('someUrl', { data: 'initialData' })
-		);
+	it('should reset error if called with different URL after error', async () => {
+		mockedAxios.get.mockRejectedValueOnce({
+			response: 'someResponse',
+		});
+		const { result, waitFor } = renderHook(() => useDataApi('someUrl', ''));
 
-		const [{ data, isLoading }] = result.current;
+		expect(mockedAxios.get).toHaveBeenCalledTimes(1);
+		expect(mockedAxios.get).toHaveBeenCalledWith('someUrl');
 
 		await waitFor(() => {
-			expect(isLoading).toBeFalsy();
-			expect(data).toStrictEqual({ data: 'someOtherData' });
-			console.log(data);
+			expect(result.current[0].error).not.toBeNull();
+			expect(result.current[0].isLoading).toBeFalsy();
+			expect(result.current[0].data).toStrictEqual('');
 		});
-		console.log(data);
+
+		const setUrl = result.current[1];
+
+		act(() => {
+			setUrl('someOtherUrl');
+		});
+
+		expect(result.current[0].isLoading).toBeTruthy();
+		expect(result.current[0].error).toBeNull();
+
+		expect(mockedAxios.get).toHaveBeenCalledTimes(2);
+		expect(mockedAxios.get).toHaveBeenCalledWith('someOtherUrl');
+
+		await waitFor(() => {
+			expect(result.current[0].isLoading).toBeFalsy();
+			expect(result.current[0].error).toBeNull();
+		});
 	});
-	it.todo('should change loading state if axios call is NOT successful');
 });
--- src/hooks/useDataApi.ts
+++ src/hooks/useDataApi.ts
@@ -1,31 +1,48 @@
-import axios from 'axios';
-import React from 'react';
+import axios, { AxiosError, AxiosResponse } from 'axios';
+import { useEffect, useState } from 'react';
 
 type Response = {
 	data: Object;
 	isLoading: boolean;
-	isError: boolean;
+	error: Error | null;
 };
 
 const useDataApi = (
 	initialUrl: string,
 	initialData: Object
 ): [response: Response, setUrl: Function] => {
-	let isLoading = true;
-	const isError = false;
-	let data = initialData;
-	const setUrl = () => {};
+	const [isLoading, setIsLoading] = useState(false);
+	const [data, setData] = useState(initialData);
+	const [error, setError] = useState<Error | null>(null);
+	const [url, setUrl] = useState<string>(initialUrl);
 
-	const fetchData = async () => {
-		if (initialUrl) {
-			await axios(initialUrl);
-			data = { data: 'someData' };
-			isLoading = false;
-		}
-	};
-	fetchData();
+	useEffect(() => {
+		const fetchData = async () => {
+			setIsLoading(true);
+			setError(null);
+			if (url) {
+				try {
+					const result: AxiosResponse = await axios.get(url);
+					setData(result.data);
+				} catch (unknownError: unknown) {
+					const axiosError: AxiosError = <AxiosError>unknownError;
+					if (axiosError.response) {
+						setError(new Error(axiosError.response.data));
+					} else if (axiosError.request) {
+						setError(
+							new Error('The request was made but no response was received.')
+						);
+					} else {
+						setError(new Error(axiosError.message));
+					}
+				}
+				setIsLoading(false);
+			}
+		};
+		fetchData();
+	}, [url]);
 
-	return [{ data, isLoading, isError }, setUrl];
+	return [{ data, isLoading, error }, setUrl];
 };
 
 export default useDataApi;
