a1a44923b7ac34553e4397f54e0613043eb32bb1
belanglos <benjamin@longstream.se>
2021-12-16 10:44:58 +0000

    Implement getPersonById.

--- __tests__/control/api.spec.ts
+++ __tests__/control/api.spec.ts
@@ -1,18 +1,34 @@
 import 'whatwg-fetch';
-import api, { searchPersonsByNameSearch } from '../../src/control/api';
+import api, {
+	searchPersonsByNameSearch,
+	getPersonById,
+} from '../../src/control/api';
 import Person from '../../src/control/Person';
 import convertPerson from '../../src/converter/Converter';
 import { DataListWrapper } from '../../src/converter/CoraData';
 import {
 	getDataListContainingFourPersons,
 	getDataListContainingOnePerson,
+	onePerson,
 } from '../../testData/searchResults';
+
+import httpClient from '../../src/control/HttpClient';
 
 jest.mock('../../src/converter/Converter');
 
 const mockConvertPerson = convertPerson as jest.MockedFunction<
 	typeof convertPerson
 >;
+
+jest.mock('../../src/control/HttpClient');
+
+const mockHttpClientGet = httpClient.get as jest.MockedFunction<
+	typeof httpClient.get
+>;
+
+beforeAll(() => {
+	mockHttpClientGet.mockResolvedValue(onePerson);
+});
 
 describe('Api', () => {
 	it('should return the dummy persons', () => {
@@ -137,6 +153,78 @@ describe('Api', () => {
 			expect(persons).toHaveLength(4);
 			expect(persons[0]).toStrictEqual(expectedPersons[0]);
 			expect(persons[3]).toStrictEqual(expectedPersons[3]);
+		});
+	});
+
+	describe('getPersonById', () => {
+		it('should exist and take an ID', () => {
+			getPersonById('someId');
+		});
+
+		it('should reject with error if empty id given and not call httpClient', async () => {
+			expect.assertions(2);
+
+			try {
+				await getPersonById('');
+			} catch (error: unknown) {
+				const castError: Error = <Error>error;
+				expect(castError.message).toStrictEqual(
+					'No id was passed to getPersonById.'
+				);
+				expect(mockHttpClientGet).toHaveBeenCalledTimes(0);
+			}
+		});
+
+		it('should correctly call httpClient', async () => {
+			const expectedUrl = 'baseUrl/record/person/someId';
+
+			expect.assertions(2);
+
+			await getPersonById('someId');
+
+			expect(mockHttpClientGet).toHaveBeenCalledTimes(1);
+			expect(mockHttpClientGet).toHaveBeenCalledWith(
+				expect.objectContaining({
+					url: expectedUrl,
+				})
+			);
+		});
+
+		it('should reject with an error if HttpClient throws error', async () => {
+			mockHttpClientGet.mockRejectedValueOnce(
+				new Error('Some error from httpClient')
+			);
+
+			expect.assertions(2);
+
+			try {
+				await getPersonById('someId');
+			} catch (error: unknown) {
+				const castError: Error = <Error>error;
+				expect(mockHttpClientGet).toHaveBeenCalledTimes(1);
+				expect(castError.message).toStrictEqual('Some error from httpClient');
+			}
+		});
+
+		it('should pass recordData received from httpClient to convertPerson', async () => {
+			expect.assertions(2);
+
+			await getPersonById('someId');
+
+			expect(mockConvertPerson).toHaveBeenCalledTimes(1);
+			expect(mockConvertPerson).toHaveBeenCalledWith(onePerson.record.data);
+		});
+
+		it('should resolve with a person if a person could be found', async () => {
+			const expectedPersons: Person[] = createMockPersons(1);
+
+			mockConvertPerson.mockReturnValueOnce(expectedPersons[0]);
+
+			expect.assertions(1);
+
+			const person: Person = await getPersonById('someId');
+
+			expect(person).toStrictEqual(expectedPersons[0]);
 		});
 	});
 });
--- src/control/api.ts
+++ src/control/api.ts
@@ -1,5 +1,7 @@
 import convertPerson from '../converter/Converter';
 import { RecordWrapper } from '../converter/CoraData';
+import httpClient from './HttpClient';
+import { IHttpClientRequestParameters } from './IHttpClient';
 import Person from './Person';
 
 const DUMMY_PERSONS: Person[] = [
@@ -37,6 +39,28 @@ export function searchPersonsByNameSearch(
 				return convertPerson(recordWrapper.record.data);
 			});
 		});
+}
+
+export function getPersonById(id: string): Promise<Person> {
+	return new Promise((resolve, reject) => {
+		if (id === '') {
+			reject(new Error('No id was passed to getPersonById.'));
+		} else {
+			const urlForPersonRecord = `${process.env.BASE_URL}record/person/${id}`;
+			const parameters: IHttpClientRequestParameters = {
+				url: urlForPersonRecord,
+			};
+			httpClient
+				.get<RecordWrapper>(parameters)
+				.then((recordWrapper) => {
+					const person = convertPerson(recordWrapper.record.data);
+					resolve(person);
+				})
+				.catch((error: unknown) => {
+					reject(error);
+				});
+		}
+	});
 }
 
 export default { getPersons };
