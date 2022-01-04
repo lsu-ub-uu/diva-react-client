03dc3babed09e83605b9c584dd3e63530fe2e41d
belanglos <benjamin@longstream.se>
2022-01-04 12:21:19 +0000

    Continue on search.

--- __tests__/control/api/searchPersonsByNameSearch.spec.ts
+++ __tests__/control/api/searchPersonsByNameSearch.spec.ts
@@ -1,6 +1,14 @@
 import httpClient from '../../../src/control/HttpClient';
+import convertPerson from '../../../src/converter/Converter';
 import searchPersonsByNameSearch from '../../../src/control/api/searchPersonByNameSearch';
 import { threePersonObjects } from '../../../testData/personData';
+import List from '../../../src/control/List';
+import Person from '../../../src/control/Person';
+import {
+	dataListContainingFourPersons,
+	dataListContainingOnePerson,
+	getDataListContainingOnePerson,
+} from '../../../testData/searchResults';
 
 jest.mock('../../../src/control/HttpClient');
 
@@ -8,10 +16,16 @@ const mockHttpClientGet = httpClient.get as jest.MockedFunction<
 	typeof httpClient.get
 >;
 
+jest.mock('../../../src/converter/Converter');
+
+const mockConvertPerson = convertPerson as jest.MockedFunction<
+	typeof convertPerson
+>;
+
 describe('searchPersonsByNameSearch', () => {
 	beforeAll(() => {
 		process.env.BASE_URL = 'baseUrl/';
-		mockHttpClientGet.mockResolvedValue(threePersonObjects);
+		mockHttpClientGet.mockResolvedValue(getDataListContainingOnePerson());
 	});
 
 	it('should exist and take searchTerm, start and rows', () => {
@@ -54,10 +68,73 @@ describe('searchPersonsByNameSearch', () => {
 		);
 	});
 
-	it.todo('should call personConverter for each person in dataList');
-	it.todo(
-		'should return List containing array of persons as well as from/to/max from dataList'
-	);
+	it('should return empty list if httpClient returns with empty dataList, convertPerson should not have been called', async () => {
+		mockHttpClientGet.mockResolvedValueOnce({
+			dataList: {
+				fromNo: '1',
+				data: [],
+				totalNo: '0',
+				toNo: '0',
+			},
+		});
+
+		expect.assertions(5);
+
+		const returnedList: List<Person> = await searchPersonsByNameSearch(
+			'someSearchTerm'
+		);
+
+		expect(returnedList.data).toHaveLength(0);
+		expect(returnedList.fromNo).toStrictEqual(1);
+		expect(returnedList.totalNo).toStrictEqual(0);
+		expect(returnedList.toNo).toStrictEqual(0);
+
+		expect(mockConvertPerson).not.toHaveBeenCalled();
+	});
+
+	it('should call convertPerson once with correct data if httpClient resolves with dataList of size 1', async () => {
+		mockHttpClientGet.mockResolvedValueOnce(dataListContainingOnePerson);
+
+		expect.assertions(2);
+
+		await searchPersonsByNameSearch('someSearchTerm');
+
+		expect(mockConvertPerson).toHaveBeenCalledTimes(1);
+		expect(mockConvertPerson).toHaveBeenCalledWith(
+			dataListContainingOnePerson.dataList.data[0].record.data
+		);
+	});
+
+	it('should call convertPerson for each person in dataList', async () => {
+		mockHttpClientGet.mockResolvedValueOnce(dataListContainingFourPersons);
+
+		expect.assertions(3);
+
+		await searchPersonsByNameSearch('someSearchTerm');
+		expect(mockConvertPerson).toHaveBeenCalledTimes(4);
+		expect(mockConvertPerson).toHaveBeenNthCalledWith(
+			1,
+			dataListContainingFourPersons.dataList.data[0].record.data
+		);
+		expect(mockConvertPerson).toHaveBeenNthCalledWith(
+			4,
+			dataListContainingFourPersons.dataList.data[3].record.data
+		);
+	});
+
+	it('should return List containing array of persons as well as from/to/max from dataList', async () => {
+		mockHttpClientGet.mockResolvedValueOnce(dataListContainingFourPersons);
+
+		expect.assertions(2);
+
+		const list: List<Person> = await searchPersonsByNameSearch(
+			'someSearchTerm'
+		);
+
+		expect(mockConvertPerson).toHaveBeenCalledTimes(4);
+
+		expect(list.data).toHaveLength(4);
+	});
 
 	it('should reject with an error if HttpClient throws error', async () => {
 		mockHttpClientGet.mockRejectedValueOnce(
--- src/control/api/searchPersonByNameSearch.ts
+++ src/control/api/searchPersonByNameSearch.ts
@@ -1,11 +1,14 @@
+import convertPerson from '../../converter/Converter';
+import { DataListWrapper, RecordWrapper } from '../../converter/CoraData';
 import httpClient from '../HttpClient';
 import { IHttpClientRequestParameters } from '../IHttpClient';
-
+import List from '../List';
+import Person from '../Person';
 function searchPersonsByNameSearch(
 	searchTerm: string,
 	start?: number,
 	rows?: number
-) {
+): Promise<List<Person>> {
 	return new Promise((resolve, reject) => {
 		if (searchTerm === '') {
 			reject(
@@ -22,13 +25,21 @@ function searchPersonsByNameSearch(
 			};
 
 			httpClient
-				.get(parameters)
-				.then(() => {
+				.get<DataListWrapper>(parameters)
+				.then((dataListWrapper) => {
 					// receives dataList from api
 					// foreach element in dataList -> call convertPerson
 					// resolve with ???
 					// - List
-					resolve('');
+					const persons: Person[] = [];
+					if (dataListWrapper.dataList.data.length > 0) {
+						const records: RecordWrapper[] = dataListWrapper.dataList.data;
+
+						records.map((recordWrapper) => {
+							persons.push(convertPerson(recordWrapper.record.data));
+						});
+					}
+					resolve(new List<Person>(persons, 1, 0, 0));
 				})
 				.catch((error: unknown) => {
 					reject(error);
