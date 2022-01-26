3b3bbd6a5a7f7288671d152cec301276c468378d
belanglos <benjamin@longstream.se>
2022-01-26 10:26:24 +0000

    Removed unused hooks.

--- src/components/PersonSearch/useRows.ts
+++
@@ -1,6 +0,0 @@
-const useRows = (rows: number, maxRows: number, defaultRows: number) => {
-	const rowsToReturn = rows >= 1 && rows <= maxRows ? rows : defaultRows;
-	return { rows: rowsToReturn };
-};
-
-export default useRows;
--- src/components/PersonSearch/useStart.ts
+++
@@ -1,8 +0,0 @@
-const MIN_START_VALUE = 1;
-
-const useStart = (start: number) => {
-	const startToReturn = start >= 1 ? start : MIN_START_VALUE;
-	return { start: startToReturn };
-};
-
-export default useStart;
