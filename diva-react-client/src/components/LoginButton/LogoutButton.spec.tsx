9babad9b33ef10087c5d364f387b9ebe045ab238
belanglos <benjamin@longstream.se>
2022-03-11 14:01:21 +0000

    Started on logoutHook.

--- diva-react-client/src/components/LoginButton/LogoutButton.spec.tsx
+++ diva-react-client/src/components/LoginButton/LogoutButton.spec.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import LogoutButton from './LogoutButton';
+import { LOGIN_STATUS, useAuth } from '../../context/AuthContext';
+import userEvent from '@testing-library/user-event';
+
+jest.mock('../../context/AuthContext');
+const mockUseAuth = useAuth as jest.MockedFunction<typeof useAuth>;
+
+beforeAll(() => {
+	mockUseAuth.mockReturnValue({
+		auth: {
+			deleteUrl: 'someDeleteUrl',
+			idFromLogin: 'someId',
+			status: LOGIN_STATUS.LOGGED_IN,
+			token: 'someToken',
+		},
+		onAuthChange: jest.fn(),
+	});
+});
 
 describe('LogoutButton.spec', () => {
 	it('It renders button "Logout"', () => {
@@ -8,4 +25,21 @@ describe('LogoutButton.spec', () => {
 
 		expect(screen.getByRole('button', { name: 'Logout' }));
 	});
+
+	it('calls useAuth', () => {
+		render(<LogoutButton />);
+
+		expect(mockUseAuth).toHaveBeenCalled();
+	});
+
+	it('if button is clicked, call fetch with auth.deleteUrl', () => {
+		render(<LogoutButton />);
+
+		clickLogoutButton();
+	});
 });
+
+const clickLogoutButton = () => {
+	const logoutButton = screen.getByRole('button', { name: 'Logout' });
+	userEvent.click(logoutButton);
+};
--- diva-react-client/src/components/LoginButton/LogoutButton.tsx
+++ diva-react-client/src/components/LoginButton/LogoutButton.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Button from '../../styles/Button';
+import { useAuth } from '../../context/AuthContext';
 
 const LogoutButton = function () {
+	useAuth();
 	return <Button type="button">Logout</Button>;
 };
 
