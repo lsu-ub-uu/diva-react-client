ca66b4dfb5a83ed4e36d06825e4b23275538320f
belanglos <benjamin@longstream.se>
2022-05-31 11:39:18 +0000

    Remove FakeLogin.

--- diva-react-client/src/App.tsx
+++ diva-react-client/src/App.tsx
@@ -10,7 +10,6 @@ import PersonSearch from './components/PersonSearch';
 import PersonPage from './components/PersonPage';
 import { AuthProvider } from './context/AuthContext';
 import AuthComponent from './components/Login/AuthComponent';
-import FakeLogin from '../fake/FakeLogin';
 
 const theme: ThemeType = {
 	global: {
@@ -71,7 +70,6 @@ const App = function () {
 				<Layout>
 					<Sidebar>
 						<AuthComponent />
-						<FakeLogin />
 					</Sidebar>
 					<Main>
 						<Routes>
