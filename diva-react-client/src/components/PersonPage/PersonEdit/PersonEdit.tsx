c306ce940ecdaf0a63d814f64324eaabc053a3ae
belanglos <benjamin@longstream.se>
2022-06-01 09:12:28 +0000

    Extract PersonDomainParts.

--- diva-react-client/src/components/PersonPage/PersonEdit/PersonEdit.tsx
+++ diva-react-client/src/components/PersonPage/PersonEdit/PersonEdit.tsx
@@ -27,6 +27,7 @@ import getDomainCollection from '../../../divaData/resources';
 import PersonViewEdit from '../PersonViewEdit';
 import { useAuth } from '../../../context/AuthContext';
 import {
+	PersonDomainpartAction,
 	PersonDomainPartActionType,
 	personDomainPartReducer,
 } from './personDomainPartReducer';
@@ -38,6 +39,7 @@ import {
 	FormPersonDomainPart,
 } from '../../../types/FormPersonDomainPart';
 import { Repeatable } from '../../../types/Repeatable';
+import { Auth } from '../../../context/types';
 
 const INVALID_YEAR_MESSAGE = 'Ange ett giltigt år';
 
@@ -252,114 +254,13 @@ const PersonEdit = function ({
 						/>
 					</Box>
 
-					{person.personDomainParts.length > 0 &&
-						person.personDomainParts.map((pdpId) => {
-							const personDomainPart = personDomainParts.find(
-								(pdp) => pdp.id === pdpId
-							);
-							if (
-								!personDomainPart ||
-								personDomainPart.domain !== auth.domain
-							) {
-								return null;
-							}
-							const title =
-								getDomainCollection().get(personDomainPart.domain) ||
-								`DomänId: ${personDomainPart.domain}`;
-							return (
-								<Box
-									margin={{ top: 'large', bottom: 'large' }}
-									key={personDomainPart.id}
-								>
-									<Heading margin="none" level="5">
-										{title}
-									</Heading>
-									{/* <Text>{personDomainPart.domain}</Text> */}
-									{Object.values(personDomainPart.affiliations).map(
-										(affiliation, index) => {
-											// const affiliation = affiliations[affiliation.id];
-											return (
-												<Card
-													// eslint-disable-next-line react/no-array-index-key
-													key={`${personDomainPart.id}-${affiliation.id}-${index}`}
-													margin={{ top: 'small', bottom: 'small' }}
-													pad="small"
-												>
-													<CardHeader pad="small">
-														{affiliation.id !== '' && (
-															<Heading margin="none" level="6">
-																{organisationMap.get(affiliation.id) ||
-																	affiliation.id}
-															</Heading>
-														)}
-														{/* {affiliation.id === '' && <OrganisationChooser />} */}
-													</CardHeader>
-													{/* <Text>{affiliation.name}</Text> */}
-													<Box direction="row" justify="between">
-														<MemoizedFormField
-															label="Från"
-															name={`${affiliation.id}-from`}
-															value={affiliation.fromYear}
-															onChange={(
-																event: React.ChangeEvent<HTMLInputElement>
-															) => {
-																dispatchPersonDomainParts({
-																	type: PersonDomainPartActionType.SET_AFFILIATION_FIELD,
-																	payload: {
-																		personDomainPartId: personDomainPart.id,
-																		affiliationId: affiliation.id,
-																		field: 'fromYear',
-																		value: event.target.value,
-																	},
-																});
-															}}
-															validate={validateWithRegex(
-																/^[0-9]{4}$/,
-																INVALID_YEAR_MESSAGE
-															)}
-														/>
-														<MemoizedFormField
-															name={`${affiliation.id}-until`}
-															label="Till"
-															value={affiliation.untilYear}
-															validate={validateWithRegex(
-																/^[0-9]{4}$/,
-																INVALID_YEAR_MESSAGE
-															)}
-															onChange={(
-																event: React.ChangeEvent<HTMLInputElement>
-															) => {
-																dispatchPersonDomainParts({
-																	type: PersonDomainPartActionType.SET_AFFILIATION_FIELD,
-																	payload: {
-																		personDomainPartId: personDomainPart.id,
-																		affiliationId: affiliation.id,
-																		field: 'untilYear',
-																		value: event.target.value,
-																	},
-																});
-															}}
-														/>
-														<TrashButton
-															plain
-															onClick={() => {
-																dispatchPersonDomainParts({
-																	type: PersonDomainPartActionType.DELETE_AFFILIATION,
-																	payload: {
-																		personDomainPartId: personDomainPart.id,
-																		affiliationId: affiliation.id,
-																	},
-																});
-															}}
-														/>
-													</Box>
-												</Card>
-											);
-										}
-									)}
-								</Box>
-							);
-						})}
+					<PersonDomainParts
+						personDomainPartIds={person.personDomainParts}
+						personDomainParts={personDomainParts}
+						auth={auth}
+						organisationMap={organisationMap}
+						dispatchPersonDomainParts={dispatchPersonDomainParts}
+					/>
 
 					<Public publicValue={person.public} dispatchPerson={dispatchPerson} />
 
@@ -388,6 +289,132 @@ const PersonEdit = function ({
 		</Grid>
 	);
 };
+
+const PersonDomainParts = React.memo(
+	({
+		personDomainPartIds,
+		personDomainParts,
+		auth,
+		organisationMap,
+		dispatchPersonDomainParts,
+	}: {
+		personDomainPartIds: string[];
+		personDomainParts: FormPersonDomainPart[];
+		auth: Auth;
+		organisationMap: Map<string, string>;
+		dispatchPersonDomainParts: (value: PersonDomainpartAction) => void;
+	}) => {
+		return (
+			<Box>
+				{personDomainPartIds.length > 0 &&
+					personDomainPartIds.map((pdpId) => {
+						const personDomainPart = personDomainParts.find(
+							(pdp) => pdp.id === pdpId
+						);
+						if (!personDomainPart || personDomainPart.domain !== auth.domain) {
+							return null;
+						}
+						const title =
+							getDomainCollection().get(personDomainPart.domain) ||
+							`DomänId: ${personDomainPart.domain}`;
+						return (
+							<Box
+								margin={{ top: 'large', bottom: 'large' }}
+								key={personDomainPart.id}
+							>
+								<Heading margin="none" level="5">
+									{title}
+								</Heading>
+								{/* <Text>{personDomainPart.domain}</Text> */}
+								{Object.values(personDomainPart.affiliations).map(
+									(affiliation, index) => {
+										// const affiliation = affiliations[affiliation.id];
+										return (
+											<Card
+												// eslint-disable-next-line react/no-array-index-key
+												key={`${personDomainPart.id}-${affiliation.id}-${index}`}
+												margin={{ top: 'small', bottom: 'small' }}
+												pad="small"
+											>
+												<CardHeader pad="small">
+													{affiliation.id !== '' && (
+														<Heading margin="none" level="6">
+															{organisationMap.get(affiliation.id) ||
+																affiliation.id}
+														</Heading>
+													)}
+													{/* {affiliation.id === '' && <OrganisationChooser />} */}
+												</CardHeader>
+												{/* <Text>{affiliation.name}</Text> */}
+												<Box direction="row" justify="between">
+													<MemoizedFormField
+														label="Från"
+														name={`${affiliation.id}-from`}
+														value={affiliation.fromYear}
+														onChange={(
+															event: React.ChangeEvent<HTMLInputElement>
+														) => {
+															dispatchPersonDomainParts({
+																type: PersonDomainPartActionType.SET_AFFILIATION_FIELD,
+																payload: {
+																	personDomainPartId: personDomainPart.id,
+																	affiliationId: affiliation.id,
+																	field: 'fromYear',
+																	value: event.target.value,
+																},
+															});
+														}}
+														validate={validateWithRegex(
+															/^[0-9]{4}$/,
+															INVALID_YEAR_MESSAGE
+														)}
+													/>
+													<MemoizedFormField
+														name={`${affiliation.id}-until`}
+														label="Till"
+														value={affiliation.untilYear}
+														validate={validateWithRegex(
+															/^[0-9]{4}$/,
+															INVALID_YEAR_MESSAGE
+														)}
+														onChange={(
+															event: React.ChangeEvent<HTMLInputElement>
+														) => {
+															dispatchPersonDomainParts({
+																type: PersonDomainPartActionType.SET_AFFILIATION_FIELD,
+																payload: {
+																	personDomainPartId: personDomainPart.id,
+																	affiliationId: affiliation.id,
+																	field: 'untilYear',
+																	value: event.target.value,
+																},
+															});
+														}}
+													/>
+													<TrashButton
+														plain
+														onClick={() => {
+															dispatchPersonDomainParts({
+																type: PersonDomainPartActionType.DELETE_AFFILIATION,
+																payload: {
+																	personDomainPartId: personDomainPart.id,
+																	affiliationId: affiliation.id,
+																},
+															});
+														}}
+													/>
+												</Box>
+											</Card>
+										);
+									}
+								)}
+							</Box>
+						);
+					})}
+			</Box>
+		);
+	}
+);
 
 const MemoizedFormField = React.memo(
 	({
