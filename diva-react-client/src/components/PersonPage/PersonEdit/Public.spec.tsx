98c7e05f83ebceef8415a82fad049545139999c8
Johan Andersson <johan.andersson@ub.uu.se>
2022-06-13 10:37:10 +0000

    Test for public radio button

---
+++ diva-react-client/src/components/PersonPage/PersonEdit/Public.spec.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Public from './Public';
+
+const mockDispatchPerson = jest.fn();
+describe('Public component', () => {
+	it('renders click public radio button', () => {
+		render(<Public publicValue="yes" dispatchPerson={mockDispatchPerson} />);
+		let publicRadioButton = screen.getByLabelText('Ja');
+		userEvent.click(publicRadioButton);
+		publicRadioButton = screen.getByLabelText('Nej');
+		userEvent.click(publicRadioButton);
+		expect(mockDispatchPerson).toHaveBeenCalledTimes(1);
+	});
+});
