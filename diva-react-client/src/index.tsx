b7feecdbd3f24ad6d01db077b76212a5c9f9f94a
egiiiiil <sixteenth.bird@gmail.com>
2023-02-02 10:13:12 +0000

    updated react, types, still need to update tests to pass

--- diva-react-client/src/index.tsx
+++ diva-react-client/src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+// import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const app = document.getElementById('root');
-ReactDOM.render(
+// const app = document.getElementById('root');
+
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+
+root.render(
 	<React.StrictMode>
 		<BrowserRouter basename={process.env.BASENAME}>
 			<App />
 		</BrowserRouter>
-	</React.StrictMode>,
-	app
+	</React.StrictMode>
 );
